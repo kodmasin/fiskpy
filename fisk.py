a3c5973b46dfbd786c59ad06661dad6dd7e9bda3
kodmasin <kodmasin@89.164.236.89>
2013-12-10 16:52:46 +0100

    new branch for v0.7

--- fisk.py
+++ fisk.py
@@ -17,7 +17,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
-VERSION = 0.6
+VERSION = 0.7
 """
 
 from uuid import uuid4
